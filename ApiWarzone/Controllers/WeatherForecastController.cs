9ca940c9bd44f2ebdc4611711eca6ae832c2f12c
LeanLarroza <leandro.larroza@hotmail.com>
2020-09-26 04:55:49 +0200

    primer commit

---
+++ ApiWarzone/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rest;
+using RestSharp;
+
+namespace ApiWarzone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public IRestResponse GetStats()
+        {
+            var client = new RestSharp.RestClient("https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/TanoLarry%25231039130/psn");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "call-of-duty-modern-warfare.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "0c68a4a275mshf0ec544c4d9aa01p1af17ajsnee7215a43e31");
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+    }
+}
